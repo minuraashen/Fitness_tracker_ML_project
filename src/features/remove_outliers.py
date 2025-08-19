8c06ab44605f97adbfe81d841ce37a27931cbb73
minuraashen <minuraashensamaramanna@gmail.com>
2025-08-19 10:05:31 +0530

    Detect outliers from using chauvenet method and local outliar factor

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -171,6 +171,9 @@ def mark_outliers_chauvenet(dataset, col, C=2):
     return dataset
 
 # Loop over all columns
+for col in outlier_columns:
+    dataset = mark_outliers_chauvenet(df, col)
+    plot_binary_outliers(dataset, col, col + "_outlier", True)
 
 
 # --------------------------------------------------------------
@@ -178,14 +181,52 @@ def mark_outliers_chauvenet(dataset, col, C=2):
 # --------------------------------------------------------------
 
 # Insert LOF function
+def mark_outliers_lof(dataset, columns, n=20):
+    """Mark values as outliers using LOF
 
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+        n (int, optional): n_neighbors. Defaults to 20.
+    
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column
+        indicating whether the value is an outlier or not.
+    """
+    
+    dataset = dataset.copy()
+
+    lof = LocalOutlierFactor(n_neighbors=n)
+    data = dataset[columns]
+    outliers = lof.fit_predict(data)
+    X_scores = lof.negative_outlier_factor_
+
+    dataset["outlier_lof"] = outliers == -1
+    return dataset, outliers, X_scores
 
 # Loop over all columns
-
+dataset, outliers, X_scores = mark_outliers_lof(df, outlier_columns)
+for col in outlier_columns:
+    plot_binary_outliers(dataset, col, "outlier_lof", True)
 
 # --------------------------------------------------------------
 # Check outliers grouped by label
 # --------------------------------------------------------------
+ 
+label = "squat"
+for col in outlier_columns:
+    dataset = mark_outliers_iqr(df[df["label"] == label], col)
+    plot_binary_outliers(dataset, col, col + "_outlier", True)
+
+label = "squat"
+for col in outlier_columns:
+    dataset = mark_outliers_chauvenet(df[df["label"] == label], col)
+    plot_binary_outliers(dataset, col, col + "_outlier", True)
+
+label = "squat"
+dataset, outliers, X_scores = mark_outliers_lof(df[df["label"] == label], outlier_columns)
+for col in outlier_columns:
+    plot_binary_outliers(dataset, col, "outlier_lof", True)
 
 
 # --------------------------------------------------------------
