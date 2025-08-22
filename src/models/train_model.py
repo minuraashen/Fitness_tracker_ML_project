cc5baee03176056058ef708d3f09079daf30c2e1
minuraashen <minuraashensamaramanna@gmail.com>
2025-08-23 02:04:52 +0530

    test train split

---
+++ src/models/train_model.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from LearningAlgorithms import ClassificationAlgorithms
+import seaborn as sns
+import itertools
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
+df = pd.read_pickle("../../data/interim/03_data_features.pkl")
+
+# --------------------------------------------------------------
+# Create a training and test set
+# --------------------------------------------------------------
+
+df_train = df.drop(["participant", "category", "set", "duration"] , axis=1)
+
+X = df_train.drop("label", axis=1)
+Y = df_train["label"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    X, Y, test_size = 0.25, random_state=42, stratify=Y
+)
+
+fig, ax = plt.subplots(figsize=(10,5))
+df_train["label"].value_counts().plot(
+    kind="bar", ax=ax, color="green", label="Total"
+)
+y_train.value_counts().plot(
+    kind="bar", ax=ax, color="lime", label="Train"
+)
+y_test.value_counts().plot(
+    kind="bar", ax=ax, color="cyan", label="Test"
+)
+plt.legend()
+plt.show()
+
+
+# --------------------------------------------------------------
+# Split feature subsets
+# --------------------------------------------------------------
+
+basic_features = ["acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z"]
+square_features = ["acc_r", "gyr_r"]
+pca_features = ["pca_1", "pca_2", "pca_3"]
+time_features = [f for f in df_train.columns if "_temp_" in f]
+freq_features = [f for f in df_train.columns if ("_freq" in f) or ("pse" in f)]
+cluster_features = ["cluster"]
+
+print("basic Features:", len(basic_features))
+print("Square Features:", len(square_features))
+print("PCA Features:", len(pca_features))
+print("Time Features:", len(time_features))
+print("Frequency Features:", len(freq_features))
+print("Cluster Features:", len(cluster_features))
+
+feature_set_1 = list(set(basic_features))
+feature_set_2 = list(set(basic_features + square_features + pca_features))
+feature_set_3 = list(set(feature_set_2 + time_features))
+feature_set_4 = list(set(feature_set_3 + freq_features + cluster_features))
+
+
+# --------------------------------------------------------------
+# Perform forward feature selection using simple decision tree
+# --------------------------------------------------------------
+
+
+
+# --------------------------------------------------------------
+# Grid search for best hyperparameters and model selection
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Create a grouped bar plot to compare the results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Select best model and evaluate results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Select train and test data based on participant
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Use best model again and evaluate results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Try a simpler model with the selected features
+# --------------------------------------------------------------
