44556d8f7ea0c9e40234709bd6c1aaf983a24fc4
minuraashen <minuraashensamaramanna@gmail.com>
2025-08-23 00:56:16 +0530

    feature engineering is done. add more features, frequency abstraction, clustering

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
+from FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 
 # --------------------------------------------------------------
@@ -132,23 +134,148 @@ subset[["acc_r", "gyr_r"]].plot(subplots=True)
 # Temporal abstraction
 # --------------------------------------------------------------
 
+df_temporal = df_sqaured.copy()
 
+numAbs = NumericalAbstraction()
+
+predictor_columns = predictor_columns + ["acc_r", "gyr_r"]
+
+# window size for compute rolling average
+ws = int(1000/200) # window size of 1s
+
+df_temporal = numAbs.abstract_numerical(
+  df_temporal,
+  predictor_columns,
+  ws,
+  "mean"
+)
+
+df_temporal = numAbs.abstract_numerical(
+  df_temporal,
+  predictor_columns,
+  ws,
+  "std"
+)
+
+# In the previous method 
+# some values are aggregated using the values not belong to that set
+
+df_temporal_list = []
+
+for s in df_temporal["set"].unique():
+  subset = df_temporal[df_temporal["set"]==s]
+  subset = numAbs.abstract_numerical(
+  subset,
+  predictor_columns,
+  ws,
+  "mean"
+  )
+
+  subset = numAbs.abstract_numerical(
+    subset,
+    predictor_columns,
+    ws,
+    "std"
+  )
+  df_temporal_list.append(subset)
+
+df_temporal = pd.concat(df_temporal_list)
+
+df_temporal.info()
+
+subset[["acc_y", "acc_y_temp_mean_ws_5", "acc_y_temp_std_ws_5"]].plot(subplots=True)
+subset[["gyr_y", "gyr_y_temp_mean_ws_5", "gyr_y_temp_std_ws_5"]].plot()
 
 # --------------------------------------------------------------
 # Frequency features
 # --------------------------------------------------------------
 
+# Since FFT algorithms used in freq abs expect discrete functions as inputs
+df_freq = df_temporal.copy().reset_index()
+df_freq.info()
+freqAbs = FourierTransformation()
+
+fs = int(1000/200)
+ws = int(2800/200)
+
+#df_freq_dir = freqAbs.abstract_frequency(df_freq, predictor_columns, ws, fs)
+
+df_freq_list = []
+
+for s in df_freq["set"].unique():
+  print(f"Apply DFT to set {s}")
+  subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+  subset = freqAbs.abstract_frequency(subset,predictor_columns,ws,fs)
+  df_freq_list.append(subset)
+
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop=True)
 
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 
+# Since we have created the new columns/features using a rolling window,
+# The row values in the same column are highly correlated.
+# We should somehow get rid of that to avoid overfitting of the models
+
+df_freq = df_freq.dropna()
+
+df_freq = df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
 
+df_cluster = df_freq.copy()
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_values = range(2,10)
+inertias = []
+
+for k in k_values:
+  subset = df_cluster[cluster_columns]
+  kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+  cluster_label = kmeans.fit_predict(subset)
+  inertias.append(kmeans.inertia_)
+
+
+plt.figure(figsize=(10,10))
+plt.plot(k_values, inertias, marker='o')
+plt.xlabel("K vales")
+plt.ylabel("Inertia")
+plt.show()
+
+# From elbow method number of clusers are 5
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+# Plot the clusters in a 3d plot
+fig = plt.figure(figsize=(15,15))
+ax = fig.add_subplot(projection="3d")
+for c in df_cluster["cluster"].unique():
+  subset = df_cluster[df_cluster["cluster"]==c]
+  ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+# Plot the clusters in a 3d plot
+fig = plt.figure(figsize=(15,15))
+ax = fig.add_subplot(projection="3d")
+for l in df_cluster["label"].unique():
+  subset = df_cluster[df_cluster["label"]==l]
+  ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=l)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
