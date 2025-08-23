ce1a75fa28c220e9e1078af3ff2b2ad3acef0a75
minuraashen <minuraashensamaramanna@gmail.com>
2025-08-23 21:09:25 +0530

    Model selection, model evaluation using accuracy score and confusion matrix

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -30,14 +30,16 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 fig, ax = plt.subplots(figsize=(10,5))
-df_train["label"].value_counts().plot(
-    kind="bar", ax=ax, color="green", label="Total"
+labels = sorted(df["label"].unique())
+
+df_train["label"].value_counts().reindex(labels).plot(
+    kind="bar", ax=ax, color="lightblue", label="Total"
 )
-y_train.value_counts().plot(
-    kind="bar", ax=ax, color="lime", label="Train"
+y_train.value_counts().reindex(labels).plot(
+    kind="bar", ax=ax, color="dodgerblue", label="Train"
 )
-y_test.value_counts().plot(
-    kind="bar", ax=ax, color="cyan", label="Test"
+y_test.value_counts().reindex(labels).plot(
+    kind="bar", ax=ax, color="royalblue", label="Test"
 )
 plt.legend()
 plt.show()
@@ -102,27 +104,287 @@ plt.show()
 # Grid search for best hyperparameters and model selection
 # --------------------------------------------------------------
 
+feature_sets = [
+  feature_set_1,
+  feature_set_2,
+  feature_set_3,
+  feature_set_4,
+  selected_features
+]
+
+feature_names = [
+  "Feature Set 1",
+  "feature Set 2",
+  "Feature Set 3",
+  "Feature Set 4",
+  "Selected Features"
+]
+
+# Grid search for chooisng best hyperparameters
+iterations = 1
+score_df = pd.DataFrame()
+
+# Loop through each model and feature set
+
+for i, f in enumerate(feature_names):
+  print("Feature Set:", i)
+  selected_train_X = x_train[feature_sets[i]]
+  selected_test_X = x_test[feature_sets[i]]
+
+  # First run non deterministic classifiers to average their scores
+  performance_test_nn = 0
+  performance_test_rf = 0
+
+  for it in range(0, iterations):
+    print("\tTraining nueral network", it)
+    (
+      class_train_y,
+      class_test_y,
+      class_train_prob_y,
+      class_test_prob_y
+    ) = learner.feedforward_neural_network(
+        selected_train_X,
+        y_train, 
+        selected_test_X,
+        gridsearch=False
+    )
+    performance_test_nn += accuracy_score(y_test, class_test_y)
+
+    print("\tTraining random forest", it)
+    (
+      class_train_y,
+      class_test_y,
+      class_train_prob_y,
+      class_test_prob_y
+    ) = learner.random_forest(
+      selected_train_X,
+      y_train,
+      selected_test_X,
+      gridsearch=True
+    )
+    performance_test_rf += accuracy_score(y_test, class_test_y)
+
+    performance_test_nn = performance_test_nn/iterations
+    performance_test_rf = performance_test_rf/iterations
+
+    # And we run our deterministic classifiers:
+    print("\tTraining KNN")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.k_nearest_neighbor(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_knn = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining decision tree")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.decision_tree(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_dt = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining naive bayes")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.naive_bayes(selected_train_X, y_train, selected_test_X)
+
+    performance_test_nb = accuracy_score(y_test, class_test_y)
+
+    # Save the results in the dataframe
+    models = ["NN", "RF", "KNN", "DT", "NB"]
+    new_scores = pd.DataFrame(
+      {
+        "model": models,
+        "feature_set": f,
+        "accuracy": [
+          performance_test_nn,
+          performance_test_rf,
+          performance_test_knn,
+          performance_test_dt,
+          performance_test_nb,
+        ]
+      }
+    )
+
+    score_df = pd.concat([score_df, new_scores])
+
 
 # --------------------------------------------------------------
 # Create a grouped bar plot to compare the results
 # --------------------------------------------------------------
 
+score_df.sort_values(by="accuracy", ascending=False)
+
+plt.figure(figsize=(10,10))
+sns.barplot(x="model", y="accuracy", hue="feature_set", data=score_df)
+plt.xlabel("Model")
+plt.ylabel("Accuracy")
+plt.ylim(0.7,1)
+plt.legend(loc="lower right")
+plt.show()
 
 # --------------------------------------------------------------
 # Select best model and evaluate results
 # --------------------------------------------------------------
+
+(
+  class_train_y, 
+  class_test_y,
+  class_train_prob_y, 
+  class_test_prob_y
+) = learner.random_forest(
+  x_train[feature_set_4],
+  y_train,
+  x_test[feature_set_4],
+  gridsearch=True
+)
+
+accuracy = accuracy_score(y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+
+cm = confusion_matrix(y_true=y_test, y_pred=class_test_y, labels=classes)
+
+plt.figure(figsize=(10,10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+  plt.text(
+    i,
+    j,
+    format(cm[i,j]),
+    horizontalalignment = "center",
+    color="white" if cm[i,j] > thresh else "black"
+  )
+
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
 
 
 # --------------------------------------------------------------
 # Select train and test data based on participant
 # --------------------------------------------------------------
 
+# Since in temporal analysis we use rolling average, 
+# There may be significat overlap between each rows.
+# So if we perform train_test_split it may not be generalize the model to unseen data
+# So we split data by users
+
+participant_df = df.drop(["category", "set","duration"], axis=1)
+
+X_train = participant_df[participant_df["participant"] != "A"].drop(["label"], axis=1)
+Y_train = participant_df[participant_df["participant"] != "A"]["label"]
+
+X_test = participant_df[participant_df["participant"] == "A"].drop(["label"], axis=1)
+Y_test = participant_df[participant_df["participant"] == "A"]["label"]
+
+X_train = X_train.drop(["participant"], axis=1)
+X_test = X_test.drop(["participant"], axis=1)
+
+fig, ax = plt.subplots(figsize=(10,5))
+labels = sorted(df["label"].unique())
+df_train["label"].value_counts().reindex(labels).plot(kind="bar", ax=ax, color="lightblue", label="Total")
+Y_train.value_counts().reindex(labels).plot(kind="bar", ax=ax, color="dodgerblue", label="Train")
+Y_test.value_counts().reindex(labels).plot(kind="bar", ax=ax, color="royalblue", label="Test")
+plt.legend()
+plt.show()
 
 # --------------------------------------------------------------
 # Use best model again and evaluate results
 # --------------------------------------------------------------
 
+(
+  class_train_y, 
+  class_test_y, 
+  class_train_prob_y, 
+  class_test_prob_y
+) = learner.random_forest(
+  X_train[feature_set_4], Y_train, X_test[feature_set_4], gridsearch=True
+)
+ 
+accuracy = accuracy_score(Y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+
+cm = confusion_matrix(y_true=Y_test, y_pred=class_test_y, labels=classes)
+
+plt.figure(figsize=(10,10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+  plt.text(
+    i,
+    j,
+    format(cm[i,j]),
+    horizontalalignment = "center",
+    color="white" if cm[i,j] > thresh else "black"
+  )
+
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
 
 # --------------------------------------------------------------
-# Try a simpler model with the selected features
+# Try nueral network model with the selected features
 # --------------------------------------------------------------
+
+
+(
+  class_train_y, 
+  class_test_y, 
+  class_train_prob_y, 
+  class_test_prob_y
+) = learner.feedforward_neural_network(
+  X_train, Y_train, X_test, gridsearch=False
+)
+ 
+accuracy = accuracy_score(Y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+
+cm = confusion_matrix(y_true=Y_test, y_pred=class_test_y, labels=classes)
+
+plt.figure(figsize=(10,10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+  plt.text(
+    i,
+    j,
+    format(cm[i,j]),
+    horizontalalignment = "center",
+    color="white" if cm[i,j] > thresh else "black"
+  )
+
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
